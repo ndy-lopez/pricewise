bffbbc24f0477fc0d43033dfb6e2b16d14e9b0a8
Andy Loredo <ndy.loredo@gmail.com>
2023-10-10 12:26:54 -0400

    Fixed div border with flex-column and changed Carousel's interval time

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <section className="px-6 md:px-20 py-24">
-        <div className="flex max-xl flex-col gap-16">
+        <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">Smart Shopping Starts Here:
               <Image
--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -18,7 +18,7 @@ const HeroCarousel = () => {
       <Carousel showThumbs={false}
         autoPlay
         infiniteLoop
-        interval={2000}
+        interval={5000}
         showArrows={false}
         showStatus={false}>
 
@@ -33,6 +33,14 @@ const HeroCarousel = () => {
           />
         )))}
       </Carousel>
+
+      <Image
+        src="assets/icons/hand-drawn-arrow.svg"
+        alt="arrow"
+        width={175}
+        height={175}
+        className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"
+      />
     </div>
   );
 }
