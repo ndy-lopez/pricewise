994708396f8422f2149e14a1a5b98bbc00a1daff
Andy Loredo <ndy.loredo@gmail.com>
2023-10-11 10:50:09 -0400

    Setting isValidLink function

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,14 +1,41 @@
 "use client"
 
-import React from 'react'
+import { FormEvent, useState } from 'react';
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    // Check if hostname contains amazon.ca
+    if (hostname.includes('amazon.ca') || hostname.includes('amazon.') || hostname.endsWith('amazon')) {
+      return true
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+
+}
 
 const Searchbar = () => {
-  const handleSubmit = () => { };
+  const [searchPrompt, setSearchPrompt] = useState("")
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    alert(isValidLink ? 'Valid link' : 'Invalid link');
+  };
 
   return (
-    <form className='flex flex-wrap gap-4 mt-12'
-      onSubmit={handleSubmit}>
-      <input type="text" placeholder="Enter product link" className='searchbar-input' />
+    <form
+      className='flex flex-wrap gap-4 mt-12'
+      onSubmit={handleSubmit}
+    >
+      <input type="text" value={searchPrompt} onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link" className='searchbar-input' />
 
       <button type="submit" className='searchbar-btn'>
         Search
