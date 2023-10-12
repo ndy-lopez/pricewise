4beb40a989f8b591af477b449aa86281cc81fa39
Andy Loredo <ndy.loredo@gmail.com>
2023-10-12 10:40:47 -0400

    Extract outOfStock, images & currency

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractCurrency, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -26,6 +27,35 @@ export async function scrapeAmazonProduct(url: string) {
 
     // Extract the product title
     const title = $(`#productTitle`).text().trim();
+
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($(".a-price-symbol"));
+
+    console.log(image);
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
---
+++ lib/utils.ts
@@ -0,0 +1,24 @@
+export function extractPrice(...elements: any) {
+  for (const element of elements) {
+    const priceText = element.text().trim();
+
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
+
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
+    }
+  }
+
+  return "";
+}
+
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
